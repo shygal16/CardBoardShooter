4ee00ac340f4db0989420a3ec9afafe10f55b404
Jeffery-Liu <liujinfeng1209@gmail.com>
2017-01-13 15:15:42 -0800

    Enemy chasing

---
+++ Assets/Cardboard/Scripts/Enemies.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Enemies : MonoBehaviour {
+
+	public float m_minDis = 1.0f;
+	public float m_speed =2.0f;
+	public Transform target;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		transform.LookAt (target);
+
+		if (Vector3.Distance (transform.position, target.transform.position) >= m_minDis) 
+		{
+			transform.position += transform.forward * m_speed * Time.deltaTime;
+		}
+			
+	}
+		
+}
